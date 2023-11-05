12329380cacc36ea40ccdbcaeac05345f34f2b0e
Rime <rimeislam672@gmail.com>
2023-11-05 17:17:57 +0600

    footer added

--- src/layOut/Pages/Home.jsx
+++ src/layOut/Pages/Home.jsx
@@ -1,11 +1,14 @@
-import Banner from "../../components/header/Banner";
+// import Banner from "../../components/header/Banner";
+
+import Footer from "../../components/Footer";
 
 
 
 const Home = () => {
     return (
         <div>
-      <Banner></Banner>
+      {/* <Banner></Banner> */}
+      <Footer></Footer>
         </div>
     );
 };
